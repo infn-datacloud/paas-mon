4c4f49c53312c6a5c512504c9e1a33730efeb848
Gioacchino Vino <gioacchino.vino@infn.it>
2025-10-02 18:07:29 +0200

    debug rally msg

--- kafka-components/src/processors/modules/providerselector/processor.py
+++ kafka-components/src/processors/modules/providerselector/processor.py
@@ -57,9 +57,9 @@ class ProviderSelectorProcessor:
                 
             # Collect messages from rally topic and aggregate them
             if topic == self.settings.KAFKA_INPUT_RALLY_TOPIC:
+                self.logger.debug(f"{messages[0]}")
+                self.logger.debug(f"{messages[-1]}")
                 msgs = [ msg.value for msg in messages if isinstance(msg.value, dict) ]
-                self.logger.debug(f"{msgs[0]}")
-                self.logger.debug(f"{msgs[-1]}")
                 self.rally_data.import_multiple_messages(msgs)
                 self.logger.debug(f"Imported {self.rally_data.size()} rally messages from topic {self.settings.KAFKA_INPUT_RALLY_TOPIC}")
             
